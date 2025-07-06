88d1bfc7043a99752eabd9d6cdd0086c8a81b6a3
jinseoplee <jjjp9966@gmail.com>
2025-07-06 23:24:37 +0900

    feat: Battle.net OAuth 설정 바인딩 클래스 추가

---
+++ backend/src/main/java/com/wowtkn/backend/config/BattleNetOAuthProperties.java
@@ -0,0 +1,15 @@
+package com.wowtkn.backend.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "battlenet.oauth")
+public class BattleNetOAuthProperties {
+    private String clientId;
+    private String clientSecret;
+}
