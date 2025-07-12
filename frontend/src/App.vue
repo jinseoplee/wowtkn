890580c9296b791fe9218e293490da7ef9161450
jinseoplee <jjjp9966@gmail.com>
2025-07-12 16:04:38 +0900

    feat: App.vue 레이아웃 구조 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,21 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div id="app-wrapper">
+    <header class="app-header">
+      <div class="container">
+        <h1>
+          <a href="/">Wowtkn</a>
+        </h1>
+      </div>
+    </header>
+
+    <main class="app-main">
+      <div class="container"></div>
+    </main>
+
+    <footer class="app-footer"></footer>
+  </div>
+</template>
 
 <style scoped></style>
