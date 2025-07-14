e3ab0657e20b17460f00975d54ba585af9313ec1
jinseoplee <jjjp9966@gmail.com>
2025-07-14 23:03:11 +0900

    feat: CORS 정책 구성

---
+++ backend/src/main/java/com/wowtkn/backend/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.wowtkn.backend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(corsProperties.getAllowedOrigins())
+                .allowedMethods("GET", "OPTIONS")
+                .allowedHeaders("Content-Type")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
