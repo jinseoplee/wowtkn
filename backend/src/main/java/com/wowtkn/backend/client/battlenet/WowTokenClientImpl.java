d510665d12a1052b1ca6f81a25efd028649c66a5
jinseoplee <jjjp9966@gmail.com>
2025-08-05 01:46:37 +0900

    feat: Battle.net WowTokenClient 구현

---
+++ backend/src/main/java/com/wowtkn/backend/client/battlenet/WowTokenClientImpl.java
@@ -0,0 +1,28 @@
+package com.wowtkn.backend.client.battlenet;
+
+import com.wowtkn.backend.client.battlenet.dto.WowTokenResponse;
+import com.wowtkn.backend.config.WowTokenProperties;
+import com.wowtkn.backend.entity.Region;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RequiredArgsConstructor
+@Component
+public class WowTokenClientImpl implements WowTokenClient {
+
+    private final WebClient webClient;
+    private final WowTokenProperties properties;
+
+    @Override
+    public WowTokenResponse getWowToken(Region region, String accessToken) {
+        String url = properties.getUrl().replace("{region}", region.getCode());
+
+        return webClient.get()
+                .uri(url)
+                .header("Authorization", "Bearer " + accessToken)
+                .retrieve()
+                .bodyToMono(WowTokenResponse.class)
+                .block();
+    }
+}
