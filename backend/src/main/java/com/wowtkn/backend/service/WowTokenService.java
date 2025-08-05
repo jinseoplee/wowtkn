8cf14839db82c11c69512a534d09e4279b6b875c
jinseoplee <jjjp9966@gmail.com>
2025-08-05 22:03:10 +0900

    feat: WoW 토큰 조회 기능 추가

--- backend/src/main/java/com/wowtkn/backend/service/WowTokenService.java
+++ backend/src/main/java/com/wowtkn/backend/service/WowTokenService.java
@@ -1,6 +1,13 @@
 package com.wowtkn.backend.service;
 
+import com.wowtkn.backend.dto.WowTokenResponse;
+import com.wowtkn.backend.entity.Region;
+
+import java.util.List;
+
 public interface WowTokenService {
 
     void saveWowToken();
+
+    List<WowTokenResponse> getWowTokens(Region region, int days);
 }
--- backend/src/main/java/com/wowtkn/backend/service/WowTokenServiceImpl.java
+++ backend/src/main/java/com/wowtkn/backend/service/WowTokenServiceImpl.java
@@ -3,6 +3,7 @@ package com.wowtkn.backend.service;
 import com.wowtkn.backend.client.battlenet.AccessTokenClient;
 import com.wowtkn.backend.client.battlenet.WowTokenClient;
 import com.wowtkn.backend.client.battlenet.dto.BattleNetWowTokenResponse;
+import com.wowtkn.backend.dto.WowTokenResponse;
 import com.wowtkn.backend.entity.Region;
 import com.wowtkn.backend.entity.WowToken;
 import com.wowtkn.backend.repository.WowTokenRepository;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -64,5 +66,17 @@ public class WowTokenServiceImpl implements WowTokenService {
 
     private int convertPrice(long price) {
         return (int) (price / COPPER_TO_GOLD_DIVISOR);
+    }
+
+    @Override
+    public List<WowTokenResponse> getWowTokens(Region region, int days) {
+        long now = System.currentTimeMillis();
+        long from = now - (days * (24 * 60 * 60 * 1000L));
+
+        List<WowToken> wowTokens = wowTokenRepository.findByRegionAndTimestampGreaterThanEqualOrderByTimestampAsc(region, from);
+
+        return wowTokens.stream()
+                .map(wowToken -> new WowTokenResponse(wowToken.getPrice(), wowToken.getTimestamp()))
+                .toList();
     }
 }
