a3be33d3ee0006cc3961341c88871235df00acdd
jinseoplee <jjjp9966@gmail.com>
2025-08-06 22:48:15 +0900

    feat: 푸터 컴포넌트 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Header from "./components/layout/Header.vue";
+import Footer from "./components/layout/Footer.vue";
 </script>
 
 <template>
   <Header />
+  <Footer />
 </template>
