cfd6eb9dab0ae7c4478c77ace2098e455f0569fb
jinseoplee <jjjp9966@gmail.com>
2025-08-17 18:10:04 +0900

    feat: HomeView 컴포넌트 추가

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,1 +1,30 @@
-<template></template>
+<script setup>
+import { useWowTokens } from "@/composables/useWowTokens";
+import WowTokenPrice from "@/components/wow/token/WowTokenPrice.vue";
+import WowTokenChart from "@/components/wow/token/WowTokenChart.vue";
+import WowTokenStats from "@/components/wow/token/WowTokenStats.vue";
+
+const regions = ["US", "EU", "KR", "TW"];
+const fetchDays = 7;
+
+const { wowTokensByRegion, currentPriceByRegion, wowTokenStats } = useWowTokens(
+  regions,
+  fetchDays
+);
+</script>
+
+<template>
+  <div class="home-view">
+    <WowTokenPrice :current-price-by-region="currentPriceByRegion" />
+    <WowTokenChart :wow-tokens-by-region="wowTokensByRegion" />
+    <WowTokenStats :wow-token-stats="wowTokenStats" />
+  </div>
+</template>
+
+<style scoped>
+.home-view {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
